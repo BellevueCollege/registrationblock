86e8715ea1f9cfbf292928b5d85374cce560ece8
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-07-08 09:56:15 -0700

    ✅ TEST: Add Unit Tests for DataAPI Model
    Restructure code to make unit tests possible, and add basic
    unit tests for all methods.

--- app/Http/Controllers/BlocksController.php
+++ app/Http/Controllers/BlocksController.php
@@ -3,29 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\StudentInfo;
+use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
+use App\DataAPI;
 
 class BlocksController extends Controller
 {
     //
     public function blockList()
     {
-        $student = new StudentInfo(session('username'));
+        /**
+         * Load from Data API
+         */
+        $dataAPI = new DataAPI(
+            new Client([ 'base_uri' => config('dataapi.baseURI')])
+        );
+        $token = $dataAPI->getToken(
+            config('dataapi.clientId'),
+            config('dataapi.clientKey')
+        );
+
+        $user = $dataAPI->getUser(
+            'local' !== config('app.env') ? session('username') : 't.test',
+            $token
+        );
+
         $data = array(
-            'userData' => $student->getUser(),
+            'userData' => $user,
             'username' => session('username'),
             'logout'   => session('logout_url'),
         );
-        if (null !== $student->getUser() && null !== $student->getUser()['blocks'])
+
+        if (null !== $user && null !== $user['blocks'])
         {
             return view('blocks/blocks', $data);
         }
-        elseif (null !== $student->getUser())
+        elseif (null !== $user)
         {
             return view('error/noBlocks', $data);
         }
         else
         {
+            Log::info( 'User '. (null !== session('username') ? session('username') : '[NOT SET]') .' logged in, had no student record');
             return view('error/notStudent', $data);
         }
 
